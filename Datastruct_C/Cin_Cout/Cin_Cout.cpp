7f07967c2eaa76b8441d64ddb2e3c6c4c6b71939
Xia <1126441731@qq.com>
2021-04-19 17:04:57 +0800

    cin/cout

---
+++ Datastruct_C/Cin_Cout/Cin_Cout.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a;
+	float b;
+	char c;
+
+	cout << "输入的格式:" << endl;
+	cin >> a >> b >> c;
+	cout << a <<' '<< b <<' '<< c << ' '<<endl;
+}
